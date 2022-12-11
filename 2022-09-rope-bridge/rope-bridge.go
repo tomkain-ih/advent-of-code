9c77517e6f30d641481641a11086728941021c24
Tom Kain <tom.kain@includedhealth.com>
2022-12-11 15:24:14 -0500

    Day 9 part 2 works on example but not input

--- 2022-09-rope-bridge/rope-bridge.go
+++ 2022-09-rope-bridge/rope-bridge.go
@@ -11,8 +11,14 @@ import (
 
 func main() {
 	//input := exampleInput()
+	//input := largeExampleInput()
 	input := input()
+
+	// Part 1
 	fmt.Println(countTailPositions(input))
+
+	// Part 2
+	fmt.Println(countMultiTailPositions(input, 9))
 	//singleMove()
 
 }
@@ -32,24 +38,55 @@ func singleMove() {
 	fmt.Println()
 }
 
+func countMultiTailPositions(input []string, tailCount int) int {
+	tailSet := make(map[string]bool)
+	headPos := GridPos{0, 0}
+	tails := generateMultiTailSlice(tailCount)
+	for _, s := range input {
+		for _, i := range explodedInstruction(s) {
+			instr := string(i)
+			headPos = headPos.move(instr)
+			lastTail := multiTailChase(headPos, tails)
+			tailSet[lastTail.string()] = true
+		}
+		fmt.Println(s)
+		fmt.Print(headPos.string())
+		for _, tail := range tails {
+			fmt.Print("; " + tail.string())
+		}
+		fmt.Println()
+	}
+	return len(tailSet)
+}
+
+func generateMultiTailSlice(count int) (result []GridPos) {
+	for i := 0; i < count; i++ {
+		result = append(result, GridPos{0, 0})
+	}
+	return
+}
+
+func multiTailChase(head GridPos, tails []GridPos) (lastTail GridPos) {
+	lastTail = head
+	for i, tail := range tails {
+		tail = tail.chase(lastTail)
+		tails[i] = tail
+		lastTail = tail
+	}
+	return
+}
+
 func countTailPositions(input []string) int {
 	tailSet := make(map[string]bool)
 	headPos := GridPos{0, 0}
 	tailPos := GridPos{0, 0}
 	tailSet[tailPos.string()] = true
 	for _, s := range input {
-		fmt.Println(s)
 		for _, i := range explodedInstruction(s) {
 			instr := string(i)
 			headPos = headPos.move(instr)
 			tailPos = tailPos.chase(headPos)
 			tailSet[tailPos.string()] = true
-			fmt.Print(instr)
-			fmt.Print(": ")
-			fmt.Print(headPos.string())
-			fmt.Print(" - ")
-			fmt.Print(tailPos.string())
-			fmt.Println()
 		}
 	}
 
@@ -72,6 +109,17 @@ R 4
 D 1
 L 5
 R 2`, "\n")
+}
+
+func largeExampleInput() []string {
+	return strings.Split(`R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`, "\n")
 }
 
 func input() (result []string) {
